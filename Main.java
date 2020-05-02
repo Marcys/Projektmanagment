4dc602eebc6613f696433e4e2ee5ba7bc6094507
Marcys <m.roschanski@hotmail.de>
2020-05-02 17:26:54 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 package de.fhswf.inf;
+import java.util.Scanner;
 
 
 public class Main
@@ -7,15 +8,38 @@ public class Main
    
    /**
     * 
-    * TODO Ausgabe von ggT
+    * TODO 
+    * Eingabe der Zahlen
+    * Ausgabe von ggT
     *
     * @param args
     */
    public static void main(String[] args)
    {
       Ggt z = new Ggt();
+      int a, b, groesser, kleiner;
+      Scanner sc = new Scanner (System.in);
        
+      System.out.println("Erste Zahl eingeben:");
+      a = sc.nextInt();
+      System.out.println("");
       
-      System.out.println(z.ggT(0, 0));
+      
+      System.out.println("Zweite Zahl eingeben:");
+      b = sc.nextInt();
+      System.out.println("");
+      
+      if (a >= b) {
+         groesser=a;
+         kleiner = b;
+      }
+      else {
+         groesser =b;
+         kleiner = a;
+         
+      }
+      
+      
+      System.out.println(z.ggT(groesser, kleiner));
    }
 }
