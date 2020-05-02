355461666fe3de5870404c0119f132b2e892e9d3
Marcys <m.roschanski@hotmail.de>
2020-05-02 17:25:36 +0200

    Update Ggt.java

--- Ggt.java
+++ Ggt.java
@@ -5,10 +5,15 @@ public class Ggt
 {
 
    public static int ggT(int a, int b) {
+      int rest;
+      do {
+         rest = a%b;
+         a = b;
+         b = rest;
+       } while(b!=0);
       
       
-      
-      return 0;
+      return a;
    }
    
    
